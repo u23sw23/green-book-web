8ba985404aa37552909066c3edb88b96ee1e7d9a
u23sw23 <162894303+u23sw23@users.noreply.github.com>
2024-11-05 15:28:31 +0800

    Update introduction_script.js

--- flaskProject/static/introduction_script.js
+++ flaskProject/static/introduction_script.js
@@ -1,28 +1,24 @@
-var stars = document.getElementsByClassName("stars")[0]
-var icons = document.getElementsByClassName("icon-pingfenxingxing")
-var vote = 0
-var score = document.getElementById('score')
-score.innerText = vote
-stars.addEventListener('click', function (evt) {
-    vote = 0
-    for (var i = 0; i < 5; i++) {
-        icons[i].style.setProperty("--v", 0)
-        if (icons[i] === evt.target) {
-            vote = i
-            for (var j = 0; j < i; j++) {
-                icons[j].style.setProperty('--v', 100)
+const stars = document.querySelectorAll('.stars span');
+const score = document.querySelector('#score');
+const ratingInput = document.querySelector('#rating');  // 获取隐藏的评分输入
 
-            }
-            var ps = evt.clientX - icons[i].getBoundingClientRect().left
-            if (ps / icons[i].offsetWidth < 0.5) {
-                icons[i].style.setProperty("--v", 50)
-                vote += 0.5
-
+stars.forEach((star, index) => {
+    star.addEventListener('click', (e) => {
+        const rect = star.getBoundingClientRect();
+        const isLeft = e.clientX - rect.left < rect.width / 2;
+        let rating = index + (isLeft ? 0.5 : 1);
+        
+        score.textContent = rating + 'stars';
+        ratingInput.value = rating;  // 将评分保存到隐藏输入框
+        
+        stars.forEach((s, i) => {
+            if (i < index) {
+                s.style.setProperty('--v', '100');
+            } else if (i === index) {
+                s.style.setProperty('--v', isLeft ? '50' : '100');
             } else {
-                icons[i].style.setProperty("--v", 100)
-                vote++
+                s.style.setProperty('--v', '0');
             }
-        }
-    }
-    score.innerText = vote
-})
+        });
+    });
+});
